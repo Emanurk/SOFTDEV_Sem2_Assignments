71cf46c53e51ff0f9af89117474ad58a6bc46c4a
Emanuel Bello <belloema000@warren.k12.in.us>
2024-01-12 11:00:33 -0500

    Assignment 1.5

--- 01_Semester 1 Project Review/Assignment/src/App.java
+++ 01_Semester 1 Project Review/Assignment/src/App.java
@@ -1,5 +1,34 @@
+import java.security.MessageDigest;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       try(Scanner input = new Scanner(System.in)){
+
+            System.out.println("Enter a number");
+            String number1 = input.next();
+            System.out.println("Enter another number");
+            String number2 = input.next();
+            int number1Int = 0;
+            int number2Int = 0;
+
+            try{
+                number1Int = Integer.parseInt(number1);
+            } catch(Exception ex){
+                number1Int = 0;
+                throw new IntegerException("number is not an int");
+            }
+            try{
+                number2Int = 0;
+                throw new IntegerException("number is not an int");
+            } catch (Exception ex) {
+                number2Int = 0;
+                throw new IntegerException("number2 is not an int");
+            }
+
+        
+        
+
+       }
     }
 }
