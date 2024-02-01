3cf6f9d40c5a7d0bc7d734da7ed2a5c2c875060e
Emanuel Bello <belloema000@warren.k12.in.us>
2024-02-01 10:52:46 -0500

    Lesson2.1

--- 01_Semester 1 Project Review/Assignment/Lesson2.1/App/src/App.java
+++ 01_Semester 1 Project Review/Assignment/Lesson2.1/App/src/App.java
@@ -1,15 +1,20 @@
-import javax.print.event.PrintJobListener;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean Property = false;
-        System.out.println(Property);
-
+    
         Property<String> myStringProperty = new Property<String>();
         myStringProperty.set("Hi");
+
         System.out.println(myStringProperty.get());
         myStringProperty.set("Hello");
+
         System.out.println(myStringProperty.get());
 
+        Property<Integer> myInteger = new Property<Integer>();
+        myInteger.set(3);
+
+        System.out.println(myInteger.get());
+        myInteger.set(2);
+        
+        System.out.println(myInteger.get());
     }
 }
